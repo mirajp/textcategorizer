c54fa18c4409f71c725deab4bb6d6f035e8e3569
mirajp <mirage.patel@gmail.com>
2016-03-11 16:38:07 -0500

    Still poor performance using document probabilities

--- textcategorizer.py
+++ textcategorizer.py
@@ -5,6 +5,7 @@
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import wordnet
+from nltk.corpus import stopwords
 from nltk.tag import _pos_tag as postagger
 from nltk.tag.perceptron import PerceptronTagger
 from math import log
@@ -16,14 +17,20 @@ stemmer = SnowballStemmer("english")
 # Use lemmatization as opposed to stemming to group words by meaning
 
 # alpha = additive smoothing factor, 0 < alpha <= 1
-alpha = 0.7
-catWords = {}
-catCounts = {}
+alpha = 1.0
+#Count number of times each word appears in each category
+wordsCount = {}
+#Count the total number of words in each category
+totalCount = {}
+#Count the number of documents in each category
+docCount = {}
+numDocuments = 0.0
 testProbs = {}
 
 trainingList = raw_input('Enter the filename of the list of training documents: ')
 testingList = raw_input('Enter the filename of the list of testing documents: ')
 predictionsList = raw_input('Enter the filename to save the predictions: ')
+print "\n"
 
 def get_wordnet_pos(treebank_tag):
 
@@ -83,11 +90,12 @@ for line in trainingList:
     trainingDoc = line[0]
     trainingCat = line[1]
     #print "File: ", trainingDoc, ", cat: ", trainingCat
-    if trainingCat not in catWords:
+    if trainingCat not in wordsCount:
         #print "Adding dictionary for category:", trainingCat
-        catWords[trainingCat] = {}
-        catCounts[trainingCat] = 0
-        testProbs[trainingCat] = 0
+        wordsCount[trainingCat] = {}
+        totalCount[trainingCat] = 0.0
+        testProbs[trainingCat] = 0.0
+        docCount[trainingCat] = 0.0
     
     trainingDoc = open(trainingDoc, "rb")
     for line in trainingDoc:
@@ -99,27 +107,31 @@ for line in trainingList:
             #cleaned_word = (words[wordIter]).lower()
             foundWord = clean_word(words[wordIter])
             
-            if len(foundWord) > 0:
-                if foundWord not in catWords[trainingCat]:
-                  catWords[trainingCat][foundWord] = alpha
-                  catCounts[trainingCat] += alpha
+            if len(foundWord) > 0 and foundWord not in stopwords.words('english'):
+                if foundWord not in wordsCount[trainingCat]:
+                  wordsCount[trainingCat][foundWord] = alpha
+                  totalCount[trainingCat] += alpha
                 
-                catWords[trainingCat][foundWord] += 1
-                catCounts[trainingCat] += 1
+                wordsCount[trainingCat][foundWord] += 1
+                totalCount[trainingCat] += 1
             
             wordIter += 1
 
+    docCount[trainingCat] += 1
+    numDocuments += 1
     trainingDoc.close()
+    if numDocuments > 100:
+        break
 
 trainingList.close()
 
 
-"""
-for category in catCounts:
-  print category, "dictionary has", catCounts[category], "total words"
-  #for uniqueword in catWords[category]:
-    #print "\t", uniqueword, "appears", catWords[category][uniqueword], "times"
-"""
+for category in totalCount:
+    print "Printing category:", category
+    for uniqueword in wordsCount[category]:
+        print "\t", category, "--", uniqueword, ":", wordsCount[category][uniqueword], "/", totalCount[category]
+    print "\n\n"
+
 
 testingList = open(testingList, "rb")
 predictionsList = open(predictionsList, "wb")
@@ -127,7 +139,7 @@ for testingDocName in testingList:
     testingDocName = (testingDocName.split())[0]
     #print "File: ", testingDoc
     testingDoc = open(testingDocName, "rb")
-    for category in catCounts:
+    for category in totalCount:
         testProbs[category] = 0
 
     for line in testingDoc:
@@ -139,26 +151,37 @@ for testingDocName in testingList:
             #cleaned_word = (words[wordIter]).lower()
             foundWord = clean_word(words[wordIter])
             
-            #Ignoring unknown words since their probabilities are insignificant
-            for category in catWords:
-                if foundWord in catWords[category]:
-                    #print "Numerator:", (catWords[category][foundWord])
-                    #print "Denominator:", (catCounts[category])
-                    #print "Fraction:", float(catWords[category][foundWord])/float(catCounts[category])
-                    testProbs[category] += log(float(catWords[category][foundWord])/float(catCounts[category]))
-            
+            for category in wordsCount:
+                if foundWord in wordsCount[category]:
+                    #print "Numerator:", (wordsCount[category][foundWord])
+                    #print "Denominator:", (totalCount[category])
+                    #print "Fraction:", float(wordsCount[category][foundWord])/float(totalCount[category])
+                    
+                    testProbs[category] += log(float(wordsCount[category][foundWord]/totalCount[category]))
+                    #testProbs[category] *= (float(wordsCount[category][foundWord]/totalCount[category]))
+                else:
+                    testProbs[category] += log(float(alpha/totalCount[category]))
+                    #testProbs[category] *= (float(alpha/totalCount[category]))
+
             wordIter += 1
+
+    for category in wordsCount:
+        print "category '", category, "' prob:", float(docCount[category]/numDocuments), "logval =", log(float(docCount[category]/numDocuments)) 
+        print "word log sum prob =", testProbs[category]
+        testProbs[category] += log(float(docCount[category]/numDocuments))
+        #testProbs[category] *= (float(docCount[category]/numDocuments))
+        print "new prob =", testProbs[category]
 
     testingDoc.close()
     maxProb = float("-inf")
     likelyCategory = ""
 
     for category in testProbs:
+        print "category '", category, "' has prob", testProbs[category]
         if testProbs[category] > maxProb:
-            #print "Switching prediction to", category
+            print "Switching prediction to", category
             likelyCategory = category
             maxProb = testProbs[category]
-            #print "New prob =", maxProb
         
     predictionsList.write(testingDocName + " " + likelyCategory + '\n')
 
