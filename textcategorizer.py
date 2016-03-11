df1609616cc19fdbd7ffe3eb8dde64d810399f42
mirajp <mirage.patel@gmail.com>
2016-03-11 15:00:10 -0500

    Poor accuracy, 14 correct, 429 incorrect

--- textcategorizer.py
+++ textcategorizer.py
@@ -3,14 +3,27 @@
 # Naive Bayes Text Categorizer
 
 from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import wordnet
 from nltk.tag import _pos_tag as postagger
 from nltk.tag.perceptron import PerceptronTagger
+from math import log
 
 tagset = None
 tagger = PerceptronTagger()
 lemmatizer = WordNetLemmatizer()
+stemmer = SnowballStemmer("english")
 # Use lemmatization as opposed to stemming to group words by meaning
+
+# alpha = additive smoothing factor, 0 < alpha <= 1
+alpha = 0.7
+catWords = {}
+catCounts = {}
+testProbs = {}
+
+trainingList = raw_input('Enter the filename of the list of training documents: ')
+testingList = raw_input('Enter the filename of the list of testing documents: ')
+predictionsList = raw_input('Enter the filename to save the predictions: ')
 
 def get_wordnet_pos(treebank_tag):
 
@@ -44,6 +57,12 @@ def clean_word(input_word):
     cleaned_word = cleaned_word.replace(")", "")
     cleaned_word = cleaned_word.replace("-", "")
 
+    #Stem
+    if len(cleaned_word) > 0:
+        cleaned_word = stemmer.stem(cleaned_word)
+
+    """
+    #Lemmatize
     if len(cleaned_word) > 0:
         #print "word =", cleaned_word
         #posTag = (nltk.tag._pos_tag([cleaned_word], tagset, tagger))[0][1]
@@ -54,27 +73,21 @@ def clean_word(input_word):
         #print "pos =", posTag
         cleaned_word = (lemmatizer.lemmatize(cleaned_word, posTag)).lower()
         #print "Changed from", words[wordIter], "to", cleaned_word
+    """
 
     return cleaned_word
-
-trainingList = raw_input('Enter the filename of the list of training documents: ')
-testingList = raw_input('Enter the filename of the list of testing documents: ')
-
-# alpha = additive smoothing factor, 0 < alpha <= 1
-alpha = 1
-catWords = {}
-catCounts = {}
 
 trainingList = open(trainingList, "rb")
 for line in trainingList:
     line = line.split()
     trainingDoc = line[0]
     trainingCat = line[1]
-    print "File: ", trainingDoc, ", cat: ", trainingCat
+    #print "File: ", trainingDoc, ", cat: ", trainingCat
     if trainingCat not in catWords:
-        print "Adding dictionary for category:", trainingCat
+        #print "Adding dictionary for category:", trainingCat
         catWords[trainingCat] = {}
         catCounts[trainingCat] = 0
+        testProbs[trainingCat] = 0
     
     trainingDoc = open(trainingDoc, "rb")
     for line in trainingDoc:
@@ -101,16 +114,53 @@ for line in trainingList:
 trainingList.close()
 
 
+"""
 for category in catCounts:
   print category, "dictionary has", catCounts[category], "total words"
   #for uniqueword in catWords[category]:
     #print "\t", uniqueword, "appears", catWords[category][uniqueword], "times"
+"""
 
 testingList = open(testingList, "rb")
-for testingDoc in testingList:
-    testingDoc = (testingDoc.split())[0]
-    print "File: ", testingDoc
-    testingDoc = open(testingDoc, "rb")
-    testingDoc.close()
+predictionsList = open(predictionsList, "wb")
+for testingDocName in testingList:
+    testingDocName = (testingDocName.split())[0]
+    #print "File: ", testingDoc
+    testingDoc = open(testingDocName, "rb")
+    for category in catCounts:
+        testProbs[category] = 0
 
+    for line in testingDoc:
+        words = line.split()
+        numWords = len(words)
+        wordIter = 0
+        while wordIter < numWords:
+            #Turns out capitalization affects POS tag
+            #cleaned_word = (words[wordIter]).lower()
+            foundWord = clean_word(words[wordIter])
+            
+            #Ignoring unknown words since their probabilities are insignificant
+            for category in catWords:
+                if foundWord in catWords[category]:
+                    #print "Numerator:", (catWords[category][foundWord])
+                    #print "Denominator:", (catCounts[category])
+                    #print "Fraction:", float(catWords[category][foundWord])/float(catCounts[category])
+                    testProbs[category] += log(float(catWords[category][foundWord])/float(catCounts[category]))
+            
+            wordIter += 1
+
+    testingDoc.close()
+    maxProb = float("-inf")
+    likelyCategory = ""
+
+    for category in testProbs:
+        if testProbs[category] > maxProb:
+            #print "Switching prediction to", category
+            likelyCategory = category
+            maxProb = testProbs[category]
+            #print "New prob =", maxProb
+        
+    predictionsList.write(testingDocName + " " + likelyCategory + '\n')
+
+predictionsList.close()
 testingList.close()
