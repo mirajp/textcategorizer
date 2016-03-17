f79bfb0268adec7213207637219ba02b2826ad28
mirajp <mirage.patel@gmail.com>
2016-03-17 19:51:55 -0400

    Selective stemming (only for corpus1) implemented

--- textcategorizer.py
+++ textcategorizer.py
@@ -2,152 +2,137 @@
 # Miraj Patel
 # Naive Bayes Text Categorizer
 
-#from nltk.stem import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
-#from nltk.corpus import wordnet
 from nltk.corpus import stopwords
-#from nltk.tag import _pos_tag as postagger
-#from nltk.tag.perceptron import PerceptronTagger
 from math import log
 
-#tagset = None
-#tagger = PerceptronTagger()
-#lemmatizer = WordNetLemmatizer()
+#By default, don't use stopwords (only use for corpus1)
+useStopWords = 0
 stemmer = SnowballStemmer("english")
-engstopwords = stopwords.words('english')
+engstopwords = stopwords.words("english")
 mystopwords = {}
 
 for i in range(0, len(engstopwords)):
     mystopwords[engstopwords[i]] = 1
 
 # alpha = additive smoothing factor, 0 < alpha <= 1
-alpha = 0.045
+alpha = 0.05
+
 #Count number of times each word appears in each category
 wordsCount = {}
+
 #Count the total number of words in each category
 totalCount = {}
+
 #Keep track of vocabulary across entire training
 vocab = {}
 vocabSize = 0
+
 #Count the number of documents in each category
 docCount = {}
 numDocuments = 0.0
 testProbs = {}
-
-trainingList = raw_input('Enter the filename of the list of training documents: ')
-testingList = raw_input('Enter the filename of the list of testing documents: ')
-predictionsList = raw_input('Enter the filename to save the predictions: ')
-#print "\n"
-
-"""
-def get_wordnet_pos(treebank_tag):
-
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return wordnet.NOUN
-"""
+trainingDocs = {}
 
 def clean_word(input_word):
+    global useStopWords
     cleaned_word = input_word.lower()
     cleaned_word = cleaned_word.replace(",", "")
     cleaned_word = cleaned_word.replace("!", "")
     cleaned_word = cleaned_word.replace("?", "")
     
     #Remove last period, but not the period in initialization token
-    if cleaned_word.find('.') == len(cleaned_word)-1:
+    if cleaned_word.find(".") == len(cleaned_word)-1:
         cleaned_word = cleaned_word[:-1]
 
     cleaned_word = cleaned_word.replace(":", "")
     cleaned_word = cleaned_word.replace(";", "")
     cleaned_word = cleaned_word.replace("\"", "")
-    cleaned_word = cleaned_word.replace("\'", "")
     cleaned_word = cleaned_word.replace("~", "")
     cleaned_word = cleaned_word.replace("`", "")
     cleaned_word = cleaned_word.replace("(", "")
     cleaned_word = cleaned_word.replace(")", "")
     cleaned_word = cleaned_word.replace("-", "")
 
-    #Remove the 't of words like can't and haven't so they are matched to the stopwords list
-    cleaned_word = cleaned_word.replace("'t", "")
+    if useStopWords == 1:
+        #Remove the 't of words like can't and haven't so they are matched to the stopwords list
+        cleaned_word = cleaned_word.replace("'t", "")
+     
+    cleaned_word = cleaned_word.replace("\'", "")
         
     #Stem
-    if len(cleaned_word) > 0:
+    if len(cleaned_word) > 0 and useStopWords == 1:
         cleaned_word = stemmer.stem(cleaned_word)
     
-
-    """
-    #Lemmatize - surprisingly a little worse performance than stemming
-    if len(cleaned_word) > 0:
-        #print "word =", cleaned_word
-        #posTag = (nltk.tag._pos_tag([cleaned_word], tagset, tagger))[0][1]
-        posTag = (postagger([cleaned_word], tagset, tagger))[0][1]
-        #print posTag
-        #posTag = (tagger.tag([cleaned_word]))[0][1]
-        posTag = get_wordnet_pos(posTag)
-        #print "pos =", posTag
-        cleaned_word = (lemmatizer.lemmatize(cleaned_word, posTag)).lower()
-        #print "Changed from", words[wordIter], "to", cleaned_word
-    """
-
     return cleaned_word
 
-trainingList = open(trainingList, "rb")
-for line in trainingList:
-    line = line.split()
-    trainingDoc = line[0]
-    trainingCat = line[1]
-    #print "File: ", trainingDoc, ", cat: ", trainingCat
-    if trainingCat not in wordsCount:
-        #print "Adding dictionary for category:", trainingCat
-        wordsCount[trainingCat] = {}
-        totalCount[trainingCat] = 0.0
-        testProbs[trainingCat] = 0.0
-        docCount[trainingCat] = 0.0
-    
-    trainingDoc = open(trainingDoc, "rb")
+def trainWithDoc(fileName, category):
+    global alpha, vocabSize, numDocuments
+    trainingDoc = open(fileName, "rb")
     for line in trainingDoc:
         words = line.split()
         numWords = len(words)
         wordIter = 0
         while wordIter < numWords:
             foundWord = clean_word(words[wordIter])
-            
-            #if len(foundWord) > 0 and foundWord not in stopwords.words('english'):
-            #if len(foundWord) > 0:
+
             if len(foundWord) > 0 and foundWord not in mystopwords:
-                totalCount[trainingCat] += 1
-                
+                totalCount[category] += 1
                 if foundWord not in vocab:
                     vocab[foundWord] = 1
                     vocabSize += 1
                  
-                if foundWord not in wordsCount[trainingCat]:
-                    wordsCount[trainingCat][foundWord] = alpha + 1
+                if foundWord not in wordsCount[category]:
+                    wordsCount[category][foundWord] = alpha + 1
                 else:
-                    wordsCount[trainingCat][foundWord] += 1    
+                    wordsCount[category][foundWord] += 1    
 
             wordIter += 1
 
-    docCount[trainingCat] += 1
+    docCount[category] += 1
     numDocuments += 1
     trainingDoc.close()
+    return
 
-trainingList.close()
+def trainNaiveBayes(trainingList):
+    global useStopWords
+    trainingListFile = open(trainingList, "rb")
+    startOfFile = trainingListFile.tell()
+    firstLine = trainingListFile.readline()
+    firstLine = firstLine.split()
+    firstCat = firstLine[1]
 
-testingList = open(testingList, "rb")
-predictionsList = open(predictionsList, "wb")
-for testingDocName in testingList:
-    testingDocName = (testingDocName.split())[0]
-    #print "File: ", testingDoc
-    testingDoc = open(testingDocName, "rb")
-    for category in totalCount:
+    firstCat = firstCat.lower()
+    #Use stopwords for only the first corpus
+    if firstCat == "str" or firstCat == "pol" or firstCat == "dis" or firstCat == "cri" or firstCat == "oth":
+        useStopWords = 1
+
+    #Reset file pointer
+    trainingListFile.seek(startOfFile)
+
+    for line in trainingListFile:
+        line = line.split()
+        trainingDoc = line[0]
+        trainingCat = line[1]
+        trainingDocs[trainingDoc] = trainingCat
+        #print "File: ", trainingDoc, ", cat: ", trainingCat
+        if trainingCat not in wordsCount:
+            #print "Adding dictionary for category:", trainingCat
+            wordsCount[trainingCat] = {}
+            totalCount[trainingCat] = 0.0
+            testProbs[trainingCat] = 0.0
+            docCount[trainingCat] = 0.0
+            
+        trainWithDoc(trainingDoc, trainingCat)
+
+    trainingListFile.close()
+    return
+
+def makePrediction(fileName):
+    global alpha, vocabSize, numDocuments
+    prediction = ""
+    testingDoc = open(fileName, "rb")
+    for category in testProbs:
         testProbs[category] = 0
 
     for line in testingDoc:
@@ -155,13 +140,10 @@ for testingDocName in testingList:
         numWords = len(words)
         wordIter = 0
         while wordIter < numWords:
-            #Turns out capitalization affects POS tag
-            #cleaned_word = (words[wordIter]).lower()
             foundWord = clean_word(words[wordIter])
 
-            #if len(foundWord) > 0:
             if len(foundWord) > 0 and foundWord not in mystopwords:
-                for category in wordsCount:
+                for category in testProbs:
                     if foundWord in wordsCount[category]:
                         testProbs[category] += log((wordsCount[category][foundWord]/(totalCount[category] + alpha*vocabSize)))
                     else:
@@ -169,21 +151,53 @@ for testingDocName in testingList:
                             
             wordIter += 1
 
-    for category in wordsCount:
+    for category in testProbs:
         testProbs[category] += log((docCount[category]/numDocuments))
         
     testingDoc.close()
-    maxProb = float("-inf")
-    likelyCategory = ""
 
+    maxProb = float("-inf")
     for category in testProbs:
         #print "category '", category, "' has prob", testProbs[category]
         if testProbs[category] > maxProb:
             #print "Switching prediction to", category
-            likelyCategory = category
+            prediction = category
             maxProb = testProbs[category]
-        
-    predictionsList.write(testingDocName + " " + likelyCategory + '\n')
 
-predictionsList.close()
-testingList.close()
+    return prediction
+
+def selectiveRetrain():
+    numRepeats = 6
+    while numRepeats > 5:
+        numRepeats = 0
+        for trainingDoc in trainingDocs:
+            trueCat = trainingDocs[trainingDoc]
+            predictedCat = makePrediction(trainingDoc)
+
+            while predictedCat != trueCat:
+                numRepeats += 1
+                trainWithDoc(trainingDoc, trueCat)
+                predictedCat = makePrediction(trainingDoc)
+        
+    return
+
+def testNaiveBayes(testingList, predictionsList):
+    global alpha, vocabSize, numDocuments
+    testingListFile = open(testingList, "rb")
+    predictionsListFile = open(predictionsList, "wb")
+    for testingDocName in testingListFile:
+        testingDocName = (testingDocName.split())[0]
+        likelyCategory = makePrediction(testingDocName)
+        predictionsListFile.write(testingDocName + " " + likelyCategory + "\n")
+
+    testingListFile.close()
+    predictionsListFile.close()
+    return
+
+trainingList = raw_input("Enter the filename of the list of training documents: ")
+testingList = raw_input("Enter the filename of the list of testing documents: ")
+predictionsList = raw_input("Enter the filename to save the predictions: ")
+trainNaiveBayes(trainingList)
+#selectiveRetrain()
+testNaiveBayes(testingList, predictionsList)
+exit()
